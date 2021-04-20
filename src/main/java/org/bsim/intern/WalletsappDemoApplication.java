41013cae97d23a2f7bdf92b224e87f5c6c94ca20
setiawanBSIM <setiawan.handoko@banksinarmas.com>
2021-04-20 13:55:28 +0700

    inisialisasi_walletsapp_demo_project

---
+++ src/main/java/org/bsim/intern/WalletsappDemoApplication.java
@@ -0,0 +1,13 @@
+package org.bsim.intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletsappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletsappDemoApplication.class, args);
+	}
+
+}
